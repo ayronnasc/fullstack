2bc564315d76f31d7ab555ee8733180c0fec4e46
Ayron Nascimento <156387838+ayronnasc@users.noreply.github.com>
2024-02-01 20:44:32 -0300

    news exercices

--- modulo06/trait/index.php
+++ modulo06/trait/index.php
@@ -1,9 +1,32 @@
 <?php 
 
 
-// Metodos que podem ser herdados em diversas; 
+// Metodos que podem ser herdados em diversas classes; 
+
+// interpretar como : 
+// conjunto de funcoes matematicas
 
 trait funcoes_matematicas
 {
+
+    public function adicionar($a, $b){
+        return $a + $b;
+    }
+    public function subtrair($a, $b){
+        return $a - $b;
+    }
+    public function mult($a, $b){
+        return $a * $b;
+    }
+    public function div($a, $b){
+        return $a / $b;
+    }
+
+}
+
+class Matematica{
+
+    use funcoes_matematicas; // incorpora na classe matematica todos os metodos do trait
+    
 
 }
