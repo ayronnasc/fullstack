f1f2bddba904b731814edf199a665ddb8812a7a1
Ayron Nascimento <156387838+ayronnasc@users.noreply.github.com>
2024-02-01 12:31:16 -0300

    termino do modulo 05

---
+++ modulo05/server/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+echo $_SERVER["PHP_SELF"]; // nome do script em execucao
+
+echo $_SERVER["SERVER_ADDR"]; // endereco ip do servidor 
+
+echo $_SERVER["SERVER_NAME"]; // nome do servidor
+
+echo $_SERVER["SERVER_SOFTWARE"]; // informacoes sobre o sistema do servidor
+
+echo $_SERVER["SERVER_PROTOCOL"]; // Informacoes sobre o protocolo usado
+
+echo $_SERVER["DOCUMENT_ROOT"]; // Pasta raiz da aplicacao
+
+
+
+
